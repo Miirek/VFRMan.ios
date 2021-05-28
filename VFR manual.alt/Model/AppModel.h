cd5b24a186b0b731775c4e7bd8f36076a51fb5c3
Mirek Novak <mirek@novaci.net>
2021-05-28 11:48:36 +0200

    adding app model class

---
+++ VFR manual.alt/Model/AppModel.h
@@ -0,0 +1,17 @@
+//
+//  AppModel.h
+//  VFR manual.alt
+//
+//  Created by Mirek Novak on 28.05.21.
+//  Copyright © 2021 Mirek Novak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VFR manual.alt/Model/AppModel.m
@@ -0,0 +1,13 @@
+//
+//  AppModel.m
+//  VFR manual.alt
+//
+//  Created by Mirek Novak on 28.05.21.
+//  Copyright © 2021 Mirek Novak. All rights reserved.
+//
+
+#import "AppModel.h"
+
+@implementation AppModel
+
+@end
